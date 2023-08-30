4a76f082ba1a48c708bd07afc580e67f55e9b2be
ZAL-X <143039973+ZAL-X@users.noreply.github.com>
2023-08-30 16:44:50 -0600

    Add files via upload

---
+++ SATemplate-main/SATemplate-main/miprimerprograma/sketch.js
@@ -0,0 +1,4 @@
+console.log("Hola");
+console.log("soy Isaac, me gustan los videojuegos y progragarlos :), tengo 16 años, voy a la preparatoria y estoy en ultimo año :)");
+console.log(":D");
+console.log("me gusta mucho programar");
